2d8a5e06f62c3cf168c3680c1af3595ba9bcf0bc
darcipons <darcimarie33@gmail.com>
2019-09-11 14:20:22 -0400

    changes made to about me page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import profile_pic from './profile_pic.png'
+import group_pic from './group_pic.png'
+import linkedin from './linkedin.png'
+import twitter from './twitter.png'
+import github from './github_white.png'
+import './About.css'
 
 
 const About= () => {
 
   return(
     <div>
-    <h1>About Me</h1>
+      <h1 className="about_name">Darci Pons</h1>
+      
+      <div className="bio">
+        <img src={profile_pic} alt="profile pic" className="profile_pic"></img>
+        <div className="bio_description">Hi, I am Darci a full stack web developer. I have a bachelor's degree from Florida State University in Psychology. 
+          I am a recent graduate from Wyncode Academy. I hope to continue to learn about software development and create some amazing projects.</div>
+      </div>
+      
+      <div>
+        <img src={group_pic} alt="group pic" className="group_pic"/>
+      </div>
+
+      <div className="contact">
+        <div>Contact Me!</div>
+      </div>
     </div>
   )
 }
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -7,13 +7,17 @@ const Projects = () => {
   return (
     <div className="all_projects">
       <div className="hangry">
-        <p>Hangry</p>
+        <div className="title">Hangry</div>
+        <a href='https://wyn-hangry.herokuapp.com/' target='_blank' rel="noopener noreferrer">
         <img src={hangry} alt='hangry' className="hangry_img"></img>
+        </a>      
       </div>
 
       <div className="happydog">
-        <p>HappyDog</p>
+        <div className="title">HappyDog</div>
+        <a href='https://happydog101.herokuapp.com/' target='_blank' rel="noopener noreferrer">
         <img src={happydog} alt='happydog' className="happydog_img"></img>
+        </a>
       </div>
     </div>
   )
